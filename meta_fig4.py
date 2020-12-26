ed47656ce716298ff048003bd5c7b327fa99b957
Benoit DE COURSON <bdecourson@master1.frontex.ens.fr>
2020-12-26 16:07:11 +0100

    bla

--- meta_fig4.py
+++ meta_fig4.py
@@ -6,14 +6,14 @@ import subprocess
 from shlex import split
 import math
 
-N = 40
+N = 16
 os.system("mkdir Results_fig4")
-PARAM = np.exp(np.linspace(0,np.log(11),160)) -1
+PARAM = np.logspace(0,11,128)-1
 
 
 for i in range(math.ceil(len(PARAM)/N)):
 	KWARGS = PARAM[N*i:N*(i+1)]
 	pickle.dump(KWARGS,open("./KWARGS_"+str(i),"wb"))
-	bash = "srun -N 1 --partition=dellgen -o logs.out python exec_fig4.py "+str(i) 
+	bash = "srun -N 1 --partition=lastgen -o logs.out python exec_fig4.py "+str(i) 
 	subprocess.Popen(bash.split(),stdout=subprocess.PIPE)	
 
